a7ea48e13d28c74553526d4774bcb448de2c70b6
Manishsoni2020 <sonimanish24163@gmail.com>
2025-04-10 15:22:10 +0530

    Core java

---
+++ Corejava/src/primitive_data_type/Main.java
@@ -0,0 +1,71 @@
+package primitive_data_type;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		//------PRIMITIVE DATA TYPES----------
+		
+		//1.byte=> FOR THE RANGE BTW 128 TO 127
+//		byte j=-23;       VALID
+//		byte b=100;       VALID
+//		byte c=127;       VALID
+//		byte a=130;       NOT VALID
+//		byte d=123.451;   NOT VALID
+//		byte e=true;      NOT VALID
+//		byte f="durga";   NOT VALID
+		
+		
+//		2.short=> FOR THE RANGE BTW -32768 TO 32768
+//		short s=32767;     VALID
+//		short c=-32768;    VALID
+//		short g=32768;     NOT VALID
+//		short s=123.456;   NOT VALID
+//		short p=true;      NOT VALID
+		
+//		3.int=> FOR THE RANGE BTW -2147483648 TO 2147483648
+//		int i=999999999;        VALID
+//		int d=-94554;           VALID
+//		int y=456846135548;     NOT VALID
+//		int u=true;             NOT VALID
+//		int p=122.655;          NOT VALID
+		
+		
+//		4.long=> FOR THE RANGE BTW -9223372036854775808 TO 9223372036854775807
+//		long b=4541212l;              VALID
+//		long c=-45618877785l;         VALID
+//		long h=false;                 NOT VALID
+//		long g=897465635.546464l;     NOT VALID
+		
+//		5.float=> FOR THE DECIMAL VALUES 5 TO 6 DECIMAL PLACES
+//		float b=452.885f;        VALID 
+//		float u=598484651;       VALID
+//		float o=true;            NOT VALID
+//		float i=-78863.665f;     VALID
+//		float c=78645.3666;      VALID
+		
+//		6.double=> ALSO FOR DEIMAL VALUES 14 TO 15 DECIMAL PLACES
+//		double b=452.885d;        VALID 
+//		double u=598484651;       VALID
+//		double o=true;            NOT VALID
+//		double i=-78863.665d;     VALID
+//		double c=78645.3666;      VALID
+		
+//		7.boolean=> NOT RANGE ONLY ALLOW TRUE AND FALSE
+//		boolean a=true;         VALID
+//		boolean b=True;         NOT VALID
+//		boolean c=false;        VALID
+//		boolean u=False;        NOT VALID
+//		boolean j=7845;         NOT VALID
+//		boolean c=8956.255;     NOT VALID
+		
+//		8.char=> FOR STORE SINGLE CHARACTERS,LETTERS,NUMBER,SYMBOLS
+//		char a='p';            VALID
+//		char s='8';            VALID
+//		char u='898';          NOT VALID
+//		char i='$';            VALID
+//		char o=7845.669;       NOT VALID
+		
+		
+		
+	}
+}
